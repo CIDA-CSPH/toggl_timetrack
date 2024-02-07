d3c110c988f20783c0b0b4009851562029a5a07f
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2024-02-07 12:50:46 -0700

    update

--- Code/03_analysis.R
+++ Code/03_analysis.R
@@ -17,21 +17,37 @@ setwd(working_directory)
 
 ### read members' time tracking data
 df <- read.xlsx('./DataProcessed/members timetrack.xlsx')%>%as_tibble(.)
+df <- df%>%select(-c(Start.date,Start.time,End.date,End.time))
 
+### hours logged
 df_log <- df%>%group_by(Week.number,id)%>%summarise(`logged(minutes)`=sum(`Duration(minutes)`))%>%arrange(Week.number,id)%>%
   mutate(`hours`=`logged(minutes)`/60)
+
+### hours on email or service
+df <- df%>%mutate(Email=case_when(str_detect(Project,regex("email|emails", ignore_case = TRUE))~T,
+                                  str_detect(Tags,regex("email|emails", ignore_case = TRUE))~T,
+                                  str_detect(Description, regex("email|emails", ignore_case = TRUE))~T,
+                                  .default = F),
+                  Service=case_when(str_detect(Project,regex("service|services", ignore_case = TRUE))~T,
+                                    str_detect(Tags,regex("service|services", ignore_case = TRUE))~T,
+                                    str_detect(Description, regex("service|services", ignore_case = TRUE))~T,
+                                    .default = F))
+
+df_email <- df%>%filter(Email==T)%>%group_by(Week.number,id)%>%
+  summarise(`email(minutes)`=sum(`Duration(minutes)`))%>%arrange(Week.number,id)%>%
+  mutate(`hours`=`email(minutes)`/60)
+
+df_service <- df%>%filter(Service==T)%>%group_by(Week.number,id)%>%
+  summarise(`service(minutes)`=sum(`Duration(minutes)`))%>%arrange(Week.number,id)%>%
+  mutate(`hours`=`service(minutes)`/60)
 
 
 ### function for plotting distribution
 plot_dist <- function(data, week){
   df <- data%>%filter(Week.number==week)
-  hist(df$hours)
+  hist(df$hours,breaks=10,xlab='hours',main = paste('Distribution of week',week,sep = ' '))
 }
 
-plot_dist(df_log, 44)
 
 
-for(i in 1:length(duration)){
-  tryCatch(hour(duration[i]),error=function(e){message(i)})
-  
-}
+
